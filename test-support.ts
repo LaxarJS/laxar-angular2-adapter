8e38b0019425c8c01f66b0fe91b7ef22da2b8dc4
Alexander Wilden <alexander.wilden@aixigo.de>
2017-01-17 11:19:27 +0100

    Added test support bundle

---
+++ test-support.ts
@@ -0,0 +1,13 @@
+/**
+ * Copyright 2017 aixigo AG
+ * Released under the MIT license.
+ * http://laxarjs.org/license
+ */
+import 'core-js/client/shim.min.js';
+import 'zone.js/dist/zone';
+import 'zone.js/dist/long-stack-trace-zone';
+import 'zone.js/dist/async-test';
+import 'zone.js/dist/fake-async-test';
+import 'zone.js/dist/sync-test';
+import 'zone.js/dist/proxy';
+import 'zone.js/dist/jasmine-patch';
